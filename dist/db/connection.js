326e305a859a426de2ec2151f485175676e45de1
Juanara21 <110938196+Juanara21@users.noreply.github.com>
2023-06-01 17:35:36 -0500

    s

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('prueba', 'root', 'Juanaraujo21', {
-    host: 'localhost',
+const DB_PASSWORD = process.env.DB_PASSWORD || 'Juanaraujo21';
+const DB_NAME = process.env.DB_NAME || 'bd_orientacion';
+const DB_USER = process.env.DB_USER || 'root';
+const DB_HOST = process.env.DB_HOST || 'localhost';
+console.log(DB_HOST, DB_NAME, DB_PASSWORD, DB_NAME);
+const sequelize = new sequelize_1.Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
     dialect: 'mysql',
 });
 exports.default = sequelize;
