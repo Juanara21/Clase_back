17d3b50bbfda429a04443f4192f461370da1f666
Juanara21 <110938196+Juanara21@users.noreply.github.com>
2023-06-02 17:23:16 -0500

    hola

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,6 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
+const dotenv_1 = __importDefault(require("dotenv"));
+// configuramos dotenv
+dotenv_1.default.config();
 const DB_PASSWORD = process.env.DB_PASSWORD || 'Juanaraujo21';
 const DB_NAME = process.env.DB_NAME || 'prueba';
 const DB_USER = process.env.DB_USER || 'root';
