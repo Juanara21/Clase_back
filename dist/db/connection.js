1b8a46ad624b1be64406f044d6094eb9b6b05382
Juanara21 <110938196+Juanara21@users.noreply.github.com>
2023-06-02 17:20:04 -0500

    env

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const DB_PASSWORD = process.env.DB_PASSWORD || 'Juanaraujo21';
-const DB_NAME = process.env.DB_NAME || 'bd_orientacion';
+const DB_NAME = process.env.DB_NAME || 'prueba';
 const DB_USER = process.env.DB_USER || 'root';
 const DB_HOST = process.env.DB_HOST || 'localhost';
 console.log(DB_HOST, DB_NAME, DB_PASSWORD, DB_NAME);
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const DB_PASSWORD = process.env.DB_PASSWORD || 'Juanaraujo21';
-const DB_NAME = process.env.DB_NAME || 'bd_orientacion';
+const DB_NAME = process.env.DB_NAME || 'prueba';
 const DB_USER = process.env.DB_USER || 'root';
 const DB_HOST = process.env.DB_HOST || 'localhost';
 console.log(DB_HOST,DB_NAME,DB_PASSWORD,DB_NAME )
